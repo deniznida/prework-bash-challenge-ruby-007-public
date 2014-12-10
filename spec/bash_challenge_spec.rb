91e7c65aef76843b7ec25da973da489ea7ea49da
deniznida <denizunat@hotmail.com>
2014-12-09 22:57:50 -0500

    all done

--- spec/bash_challenge_spec.rb
+++ spec/bash_challenge_spec.rb
@@ -1,49 +1,49 @@
 describe "Bash Challenge" do
   context 'Navigation' do
     it 'determines what the current working directory is' do
-      your_answer = __ # replace the __ with your answers in "quotes"
+      your_answer = "pwd"
       expect(your_answer).to eq(answer_1) 
     end
 
     it 'changes directory up one level' do 
-      your_answer = __
+      your_answer = "cd .."
       expect(your_answer).to eq(answer_2)
     end
 
     it 'lists the contents of the working directory' do 
-      your_answer = __
+      your_answer = "ls"
       expect(your_answer).to eq(answer_3)
     end
 
     it 'brings us back to the home directory' do 
-      your_answer = __
+      your_answer = "cd ~"
       expect(your_answer).to eq(answer_4)
     end
   end
 
   context 'Commands' do 
     it 'it creates a new filed called "text.html"' do
-      your_answer = __
+      your_answer = "touch text.html"
       expect(your_answer).to eq(answer_5)
     end
 
     it 'lists all files with their information in a human readable format' do 
-      your_answer = __
+      your_answer = "ls -lah"
       expect(your_answer).to eq(answer_6)
     end
 
     it 'opens a file in its default format' do 
-      your_answer = __
+      your_answer = "open"
       expect(your_answer).to eq(answer_7)
     end
 
     it 'shows us what our path is' do 
-      your_answer = __
+      your_answer = "echo $PATH"
       expect(your_answer).to eq(answer_8)
     end
 
     it 'makes a new directory called "code"' do 
-      your_answer = __
+      your_answer = "mkdir code"
       expect(your_answer).to eq(answer_9)
     end
   end
